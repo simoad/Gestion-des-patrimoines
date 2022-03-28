9c7cb972e0a142aca7a82451f14381270419954f
mohamed abouabdellah <abouabdellah2000@gmail.com>
2022-03-28 19:10:57 +0000

    adding pagination to bien

--- frontend/src/pages/BienList.js
+++ frontend/src/pages/BienList.js
@@ -1,6 +1,4 @@
-import { filter } from 'lodash';
 import axios from 'axios';
-import { sentenceCase } from 'change-case';
 import { useState, useEffect } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 // material
@@ -8,28 +6,93 @@ import {
   Card,
   Table,
   Stack,
-  Avatar,
   Button,
-  Checkbox,
   TableRow,
   TableBody,
   TableCell,
   Container,
   Typography,
   TableContainer,
-  TablePagination
+  TableSortLabel,
+  TableHead, TableFooter,
+  TablePagination 
 } from '@mui/material';
+//
+import PropTypes from 'prop-types';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import FirstPageIcon from '@mui/icons-material/FirstPage';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import LastPageIcon from '@mui/icons-material/LastPage';
 // components
 import Page from '../components/Page';
-import Label from '../components/Label';
 import Scrollbar from '../components/Scrollbar';
 import Iconify from '../components/Iconify';
-import SearchNotFound from '../components/SearchNotFound';
-import { UserListHead, UserListToolbar, UserMoreMenu } from '../sections/@dashboard/user';
-//
-import USERLIST from '../_mocks_/user';
-
+import BienMoreMenu from './BienMoreMenu';
 // ----------------------------------------------------------------------
+
+function TablePaginationActions(props) {
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onPageChange } = props;
+
+  const handleFirstPageButtonClick = (event) => {
+    onPageChange(event, 0);
+  };
+
+  const handleBackButtonClick = (event) => {
+    onPageChange(event, page - 1);
+  };
+
+  const handleNextButtonClick = (event) => {
+    onPageChange(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (event) => {
+    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+  return (
+    <Box sx={{ flexShrink: 0, ml: 2.5 }}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton
+        onClick={handleBackButtonClick}
+        disabled={page === 0}
+        aria-label="previous page"
+      >
+        {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </Box>
+  );
+}
+
+TablePaginationActions.propTypes = {
+  count: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired,
+};
 
 const TABLE_HEAD = [
   { id: 'code_barre', label: 'Code Barre', alignRight: false },
@@ -46,6 +109,22 @@ const TABLE_HEAD = [
 export default function BienList() {
 
   const [Biens, setBiens] = useState([]);
+
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - Biens.length) : 0;
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
 
   const getBiens = async () => {
     const res = await axios.get('http://127.0.0.1:8000/api/bien');
@@ -75,28 +154,33 @@ export default function BienList() {
         </Stack>
 
         <Card>
-          
-
           <Scrollbar>
             <TableContainer sx={{ minWidth: 800 }}>
               <Table>
-                <UserListHead
-                  
-                  headLabel={TABLE_HEAD}
-                  rowCount={USERLIST.length}
-                  
-                />
+                <TableHead>
+                  <TableRow>
+                    {TABLE_HEAD.map((headCell) => (
+                      <TableCell
+                        key={headCell.id}
+                        align={headCell.alignRight ? 'right' : 'left'}>
+                          <TableSortLabel hideSortIcon>
+                            {headCell.label}
+                          </TableSortLabel>
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                </TableHead>
                 <TableBody>
-                  {Biens
-                    .map((row) => {
-                         return (
+                  {(rowsPerPage > 0
+                      ? Biens.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                      : Biens
+                    ).map((row) => 
+                         (
                         <TableRow
                           hover
                           key={row.code_barre}
                           tabIndex={-1}
-                          role="checkbox"
                         >
-                        <TableCell align="left"/>
                           <TableCell align="left">{row.code_barre}</TableCell>
                           <TableCell align="left">{row.nom}</TableCell>
                           <TableCell align="left">{row.id_categorie}</TableCell>
@@ -105,19 +189,42 @@ export default function BienList() {
                             {row.duree_de_vie}
                           </TableCell>
                           <TableCell align="left">
-                            <UserMoreMenu />
+                            <BienMoreMenu />
                           </TableCell>
                         </TableRow>
-                      );
-                    })}
+                      )
+                    )}
                   
+                    {emptyRows > 0 && (
+                      <TableRow style={{ height: 53 * emptyRows }}>
+                        <TableCell colSpan={6} />
+                      </TableRow>
+                    )}
+
                 </TableBody>
-                
+                <TableFooter>
+                  <TableRow>
+                    <TablePagination
+                      rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                      colSpan={3}
+                      count={Biens.length}
+                      rowsPerPage={rowsPerPage}
+                      page={page}
+                      SelectProps={{
+                        inputProps: {
+                          'aria-label': 'rows per page',
+                        },
+                        native: true,
+                      }}
+                      onPageChange={handleChangePage}
+                      onRowsPerPageChange={handleChangeRowsPerPage}
+                      ActionsComponent={TablePaginationActions}
+                    />
+                  </TableRow>
+                </TableFooter>
               </Table>
             </TableContainer>
           </Scrollbar>
-
-         
         </Card>
       </Container>
     </Page>
